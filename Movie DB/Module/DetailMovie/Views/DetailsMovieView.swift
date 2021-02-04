05e8d2a10099b01825fbc5f0cc5591e00877af6a
telkomdev-rifqi <rifqi.fadhlillah@gmail.com>
2021-02-04 22:58:57 +0700

    delete dummy data

--- Movie DB/Module/DetailMovie/Views/DetailsMovieView.swift
+++ Movie DB/Module/DetailMovie/Views/DetailsMovieView.swift
@@ -40,7 +40,7 @@ struct DetailsMovieView: View {
           .frame(width: 35, height: 35, alignment: .center)
       }
       
-      Text(detailMovie.overview)
+      Text(plot)
         .font(.caption)
         .padding(.vertical, 5)
     }
@@ -48,3 +48,5 @@ struct DetailsMovieView: View {
     .padding(.vertical, 10)
   }
 }
+
+var plot = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
